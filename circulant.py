a83d7969a8eacc128b2d5f10bc89a8726000e50e
JHopeCollins <jhc.jss@gmail.com>
2022-04-19 13:47:27 +0100

    eigenvals/vecs for circulant matrices

--- circulant.py
+++ circulant.py
@@ -3,15 +3,66 @@ import numpy as np
 import scipy.fft as fft
 import scipy.linalg as linalg
 
-def eigenvalues( c ):
-    return fft.fft(c,norm='backward')/c.shape[-1]
+# construct (alpha-)circulant matrix
+def circulant( c, alpha=None ):
+    if alpha==None: # standard circulant matrix
+        return linalg.circulant(c)
 
-def eigenvectors( c ):
-    return linalg.dft(c.shape[-1])
+    else: # alpha-circulant matrix
+        raise NotImplemented( "construction of alpha-circulant matrix not implemented" )
 
-def matmul( c, r ):
-    rnew = fft.fft(r,norm='backward')
+# solve Cx=b for (alpha-)circulant matrix
+def solve( c, b, alpha=None ):
+    if alpha==None: # standard circulant matrix
+        return linalg.solve_circulant(c,b)
+
+    else: # alpha-circulant matrix
+        raise NotImplemented( "solve of alpha-circulant matrix not implemented" )
+
+# diagonal matrix Gamma_alpha for alpha-circulant diagonalisation
+def gamalph( n, alpha ):
+    return alpha**(np.arange(n)/n)
+
+# eigenvalues of (alpha-)circulant matrix
+def eigenvalues( c, alpha=None ):
+    n = c.shape[0]
+    norm='backward' # 1/n scaling is in eigenvectors
+
+    if alpha==None: # standard circulant matrix
+        return fft.fft( c, norm=norm )
+
+    else: # alpha-circulant matrix
+        gamma = gamalph(n,alpha)
+        return fft.fft( gamma*c, norm=norm )
+
+# eigenvectors of (alpha-)circulant matrix
+def eigenvectors( c, inverse=False, alpha=None ):
+    n = c.shape[0]
+    norm='sqrtn' # 1/sqrt(n) scaling on eigenvectors
+    F = linalg.dft(n,scale=norm)
+
+    if alpha==None: # standard circulant matrix
+        if inverse:
+            return F
+        else:
+            return F.conj().T
+
+    else: # alpha-circulant matrix
+        gamma = gamalph(n,alpha)
+        if inverse:
+            return np.matmul( F, np.diag(gamma) )
+        else:
+            return np.matmul( np.diag(1./gamma), F.conj().T )
+
+# multiplication of vector by (alpha-)circulant matrix
+def vecmul( c, r, alpha=None ):
+    if alpha!=None:
+        raise NotImplemented( "vector multiplication of alpha-circulant matrix not implemented yet" )
+
+    norm='ortho' # 1/sqrt(n) scaling on eigenvectors
+
+    rnew = fft.fft(r,norm=norm)
     rnew*= eigenvalues(c)
-    rnew = fft.ifft(rnew,norm='forward')
+    rnew = fft.ifft(rnew,norm=norm)
     return rnew.real
 
