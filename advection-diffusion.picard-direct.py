4ebb9dc410bfc214caf66460906d075ee6aade7f
JHopeCollins <jhc.jss@gmail.com>
2022-04-20 16:48:47 +0100

    seperate script for parallel solution: picard iteration with direct solve for each iteration

--- advection-diffusion.picard-direct.py
+++ advection-diffusion.picard-direct.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 #
 # Solve the linear advection diffusion equation
 #   using parallel timestepping
-#   and solving the all-at-once system directly
+#   and solving the stationary iteration (equation 1.4)
+#   each iteration is solved directly
 #
 # ADE solved on a periodic mesh using:
 #   time discretisation: implicit theta-method
@@ -53,8 +54,6 @@ cfl_u = u*dt/dx
 print( "nu, dt, cfl_v, cfl_u" )
 print(  nu, dt, cfl_v, cfl_u  )
 
-### === --- set up --- === ###
-
 # mesh
 x = mesh.periodic_mesh( xl=-lx/2, xr=lx/2, nx=nx )
 
@@ -86,18 +85,30 @@ b1 = np.zeros(nt)
 b1[0] =  1/dt
 b1[1] = -1/dt
 B1 = linalg.toeplitz(b1,zeros)
+C1 = circ.circulant(b1,alpha=alpha)
+D1 = circ.eigenvalues(b1,alpha=alpha)
 
 b2 = np.zeros(nt)
 b2[0] =   theta
 b2[1] = 1-theta
 B2 = linalg.toeplitz(b2,zeros)
+C2 = circ.circulant(b2,alpha=alpha)
+D2 = circ.eigenvalues(b2,alpha=alpha)
 
 # all-at-once system
 
+# actual operators
 B1M = np.kron(B1,Mfull)
 B2K = np.kron(B2,Kfull)
 
+# approximate operators
+C1M = np.kron(C1,Mfull)
+C2K = np.kron(C2,Kfull)
+
 A = B1M + B2K
+P = C1M + C2K
+
+dA = P - A
 
 # right-hand-side
 
@@ -108,18 +119,48 @@ for i in range(0,nt): b[i,:]=forcing[:]
 # initial condition of ODE becomes boundary condition for all-at-once system
 b[0,:]+=circ.vecmul( M/dt - (1-theta)*K, qinit)
 
-b = b.reshape(nt*nx)
-
 ### === --- parallel solve --- === ###
 
-# direct all-at-once solve
+# current guess for solution at each timestep
+qcurr = np.zeros( (nt, nx) )
 
-qparallel = linalg.solve(A,b)
-qparallel = qparallel.reshape(nt,nx)
+for i in range(0,nt):
+    qcurr[i,:] = qinit[:]
 
-### === --- plotting --- === ###
+# new guess for solution at each timestep
+qnext = qcurr.copy()
 
-# initial condition
+# stationary iteration (eq 1.4) with direct Pu=b solve
+
+b = b.reshape(nt*nx)
+qcurr = qcurr.reshape(nt*nx)
+qnext = qcurr.reshape(nt*nx)
+
+rhs = np.zeros_like(b)
+
+niters=4
+print( "stationary iteration with direct solve:" )
+for j in range(0,niters):
+
+    # rhs = (P-A)qk + b
+    rhs = np.matmul(dA,qcurr) + b
+    qnext = linalg.solve(P,rhs)
+
+    # test convergence
+    res = np.sum( (qnext-qcurr)*(qnext-qcurr) )/(nx*nt)
+    print( f" iteration: {j} | residual: {res}" )
+
+    # copy over guess for next iteration
+    qcurr[:] = qnext[:]
+
+print()
+
+rhs   = rhs.reshape(nt,nx)
+qcurr = qcurr.reshape(nt,nx)
+qnext = qnext.reshape(nt,nx)
+qparrallel = qnext.copy()
+
+# plotting
 plt.plot(x,qinit,color='black',label='i')
 
 # serial solution
@@ -128,10 +169,9 @@ for i in range(0,nt): plt.plot(x,qserial[i], linestyle=":", label="s"+str(i))
 
 # parallel solution
 plt.gca().set_prop_cycle(None)
-for i in range(0,nt): plt.plot(x,qparallel[i],linestyle='--', label="p"+str(i))
+for i in range(0,nt): plt.plot(x,qparrallel[i],linestyle='-.',  label="p"+str(i))
 
 plt.grid()
 plt.legend(loc='center left')
-
 plt.show()
 
