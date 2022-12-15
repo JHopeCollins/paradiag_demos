65567ac687d48686da828cb2b0ae1c6a09c20440
JHopeCollins <jhc.jss@gmail.com>
2022-12-15 18:50:28 +0000

    more operators and tests

--- circ.py
+++ circ.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy import linalg
 from scipy.fft import fft, ifft
 import scipy.sparse.linalg as spla
+from operator import add
+from functools import reduce
 
 
 def circulant(c, alpha=1):
@@ -24,12 +26,18 @@ class ToeplitzLinearOperator(spla.LinearOperator):
         self.dtype = col.dtype
 
         if inverse:
-            self.op = linalg.solve_toeplitz
+            self.op = self._solve
         else:
-            self.op = linalg.matmul_toeplitz
+            self.op = self._matmul
+
+    def _matmul(self, v):
+        return linalg.matmul_toeplitz(self.mat, v)
+
+    def _solve(self, v):
+        return linalg.solve_toeplitz(self.mat, v)
 
     def _matvec(self, v):
-        return self.op(self.mat, v)
+        return self.op(v)
 
 
 class CirculantLinearOperator(spla.LinearOperator):
@@ -40,29 +48,21 @@ class CirculantLinearOperator(spla.LinearOperator):
         self.dtype = col.dtype
         self.shape = tuple((len(col), len(col)))
 
-        if inverse:
-            self.op = self._solve
-        else:
-            self.op = self._matmul
+        if inverse: self.op = self._solve
+        else: self.op = self._matmul
 
         self.eigvals = fft(col, norm='backward')
 
     def _matmul(self, v):
-        y = ifft(fft(v)*self.eigvals)
-        if self.is_complex:
-            return y
-        else:
-            return y.real
+        return ifft(fft(v)*self.eigvals)
 
     def _solve(self, v):
-        y = ifft(fft(v)/self.eigvals)
-        if self.is_complex:
-            return y
-        else:
-            return y.real
+        return ifft(fft(v)/self.eigvals)
 
     def _matvec(self, v):
-        return self.op(v)
+        y = self.op(v)
+        if self.is_complex: return y
+        else: return y.real
 
 
 class AlphaCirculantLinearOperator(spla.LinearOperator):
@@ -87,24 +87,134 @@ class AlphaCirculantLinearOperator(spla.LinearOperator):
         self.eigvals = fft(col*self.gamma, norm='backward')
 
     def _matmul(self, v):
-        y = self._from_eigvecs(self._to_eigvecs(v)*self.eigvals)
-        if self.is_complex:
-            return y
-        else:
-            return y.real
+        return self._from_eigvecs(self._to_eigvecs(v)*self.eigvals)
 
     def _solve(self, v):
-        y = self._from_eigvecs(self._to_eigvecs(v)/self.eigvals)
+        return self._from_eigvecs(self._to_eigvecs(v)/self.eigvals)
+
+    def _matvec(self, v):
+        y = self.op(v)
         if self.is_complex:
             return y
         else:
             return y.real
-
-    def _matvec(self, v):
-        return self.op(v)
 
     def _to_eigvecs(self, v):
         return fft(v*self.gamma)
 
     def _from_eigvecs(self, v):
         return ifft(v)/self.gamma
+
+
+class BlockToeplitzLinearOperator(spla.LinearOperator):
+    def __init__(self, nblocks, block0, block1):
+        self.block0 = block0
+        self.block1 = block1
+
+        self.nblocks = nblocks
+        self.block_dim = block0.shape[0]
+
+        self.dim = self.nblocks*self.block_dim
+        self.shape = tuple((self.dim, self.dim))
+
+    def _matvec(self, v):
+        v = v.reshape((self.nblocks, self.block_dim))
+        w = np.zeros_like(v)
+
+        w[0]+= self.block1.matvec(v[0])
+        for b in range(1, self.nblocks):
+            w[b]+= self.block0.matvec(v[b-1])
+            w[b]+= self.block1.matvec(v[b])
+
+        return w.reshape(self.dim)
+
+
+class BlockLinearOperator(spla.LinearOperator):
+    def __init__(self, coeffs, blocks):
+        assert len(blocks) == len(coeffs)
+        assert len(set((b.dtype for b in blocks)))
+        assert len(set((b.shape for b in blocks)))
+        self.coeffs = coeffs
+        self.blocks = blocks
+        self.shape = blocks[0].shape
+        self.dtype = blocks[0].dtype
+        self.A = reduce(add, (c*b for c,b in zip(self.coeffs, self.blocks)))
+
+    def update_coeffs(self, coeffs):
+        self.coeffs = coeffs
+        self.A = reduce(add, (c*b for c,b in zip(self.coeffs, self.blocks)))
+
+    def _matvec(self, v):
+        return linalg.solve(self.A, v)
+
+
+class BlockCirculantLinearOperator(spla.LinearOperator):
+    def __init__(self, col0, col1, block_op, block_dim):
+        self.nblocks = len(col0)
+        self.block_dim = block_dim
+        self.dim = self.nblocks*self.block_dim
+        self.shape = tuple((self.dim, self.dim))
+        self.dtype = col0.dtype
+
+        eigvals0 = fft(col0, norm='backward')
+        eigvals1 = fft(col1, norm='backward')
+        eigvals = zip(eigvals0, eigvals1)
+
+        self.blocks = tuple((block_op(l1, l2)
+                             for l1, l2 in eigvals))
+
+    def _to_eigvecs(self, v):
+        return fft(v, axis=0)
+
+    def _from_eigvecs(self, v):
+        return ifft(v, axis=0)
+
+    def _block_solve(self, v):
+        for i in range(self.nblocks):
+            v[i] = self.blocks[i].matvec(v[i])
+        return v
+
+    def _matvec(self, v):
+        y = v.reshape((self.nblocks, self.block_dim))
+        y = self._to_eigvecs(y)
+        y = self._block_solve(y)
+        y = self._from_eigvecs(y)
+        return y.reshape(self.dim).real
+
+
+class BlockAlphaCirculantLinearOperator(spla.LinearOperator):
+    def __init__(self, col0, col1, block_op, block_dim, alpha):
+        self.nblocks = len(col0)
+        self.block_dim = block_dim
+        self.dim = self.nblocks*self.block_dim
+        self.shape = tuple((self.dim, self.dim))
+        self.dtype = col0.dtype
+
+        self.gamma = alpha**(np.arange(self.nblocks)/self.nblocks)
+
+        eigvals0 = fft(col0*self.gamma, norm='backward')
+        eigvals1 = fft(col1*self.gamma, norm='backward')
+        eigvals = zip(eigvals0, eigvals1)
+
+        self.blocks = tuple((block_op(l1, l2)
+                             for l1, l2 in eigvals))
+
+    def _to_eigvecs(self, v):
+        y = np.matmul(np.diag(self.gamma), v)
+        return fft(y, axis=0)
+
+    def _from_eigvecs(self, v):
+        y = ifft(v, axis=0)
+        return np.matmul(np.diag(1/self.gamma), y)
+
+    def _block_solve(self, v):
+        for i in range(self.nblocks):
+            v[i] = self.blocks[i].matvec(v[i])
+        return v
+
+    def _matvec(self, v):
+        y = v.reshape((self.nblocks, self.block_dim))
+        y = self._to_eigvecs(y)
+        y = self._block_solve(y)
+        y = self._from_eigvecs(y)
+        return y.reshape(self.dim).real
--- test_circ.py
+++ test_circ.py
@@ -81,3 +81,111 @@ def test_alpha_circulant_operator(alpha, col):
 
     assert np.allclose(x, linalg.solve(C, b))
     assert np.allclose(cmat.matvec(x).real, b)
+
+
+def test_block_toeplitz_operator():
+    np.random.seed(12345)
+    nb = 5
+    col = np.asarray((2,5,4,0,1), dtype=float)
+
+    col0 = col.copy()
+    col1 = col.copy()
+
+    col0[0]-=1
+    col1[0]+=1
+
+    block0 = circ.CirculantLinearOperator(col0)
+    block1 = circ.CirculantLinearOperator(col1)
+
+    tbmat = circ.BlockToeplitzLinearOperator(nb, block0, block1)
+
+    bmat0 = circ.circulant(col0)
+    bmat1 = circ.circulant(col1)
+
+    Tb =  np.kron(np.eye(nb, k=-1), bmat0) + np.kron(np.eye(nb, k=0), bmat1)
+
+    b = np.random.randn(Tb.shape[0])
+
+    x = tbmat.matvec(b)
+
+    assert np.allclose(x, np.matmul(Tb, b))
+
+
+def test_block_circulant_operator():
+    np.random.seed(12345)
+
+    block_col0 = np.asarray((2,5,4,0,1), dtype=float)
+    block_col1 = np.asarray((-1,3,0,-2,0), dtype=float)
+
+    def block_op(l0, l1):
+        col = l0*block_col0 + l1*block_col1
+        return circ.CirculantLinearOperator(col)
+
+    block = circ.circulant(block_col0 + block_col1)
+
+    col0 = np.zeros(4)
+    col0[0] = 1
+    col0[1] = -1
+
+    col1 = np.zeros(4)
+    col1[0] = 0.75
+    col1[1] = 0.25
+
+    Cop = circ.BlockCirculantLinearOperator(col0, col1, block_op, len(block_col0))
+
+    Cmat = np.kron(circ.circulant(col0), circ.circulant(block_col0)) \
+          +np.kron(circ.circulant(col1), circ.circulant(block_col1))
+
+    b = np.random.randn(Cmat.shape[0])
+
+    x = Cop.matvec(b)
+
+    assert np.allclose(x, np.matmul(Cmat, b))
+
+
+def test_block_alpha_circulant_operator():
+    np.random.seed(12345)
+
+    alpha = 0.1
+    block_col0 = np.asarray((2,5,0,0,0), dtype=float)
+    block_col1 = np.asarray((-1,3,0,0,0), dtype=float)
+    #block_col0 = np.asarray((2,5), dtype=float)
+    #block_col1 = np.asarray((-1,3), dtype=float)
+
+    def block_op(l0, l1):
+        col = l0*block_col0 + l1*block_col1
+        return circ.CirculantLinearOperator(col)
+
+    block = circ.circulant(block_col0 + block_col1)
+
+    col0 = np.zeros(4)
+    col0[0] = 1
+    col0[1] = -1
+
+    col1 = np.zeros(4)
+    col1[0] = 0.75
+    col1[1] = 0.25
+
+    Cop = circ.BlockAlphaCirculantLinearOperator(col0, col1, block_op, len(block_col0), alpha)
+
+    Cmat = np.kron(circ.circulant(col0, alpha), circ.circulant(block_col0)) \
+          +np.kron(circ.circulant(col1, alpha), circ.circulant(block_col1))
+
+    n = Cmat.shape[0]
+
+    b = np.random.randn(n)
+    xt = Cop.matvec(b)
+    xc = np.matmul(Cmat, b)
+
+    # for i in range(n):
+    #     b = np.zeros(n)
+    #     b[i] = 1
+
+    #     xt = Cop.matvec(b)
+    #     xc = np.matmul(Cmat, b)
+
+    #     print(b)
+    #     print(xt)
+    #     print(xc)
+    #     print("")
+    assert np.allclose(xt, xc)
